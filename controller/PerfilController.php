c2f76385000204bc42843fa26a27a26aa1852f17
megatron1983 <maxiprograma83@gmail.com>
2024-04-25 19:44:37 -0300

    first commit

--- controller/PerfilController.php
+++ controller/PerfilController.php
@@ -1,7 +1,24 @@
 <?php
+class PerfilController extends ControladorBase{
+    public $conectar;
 
-function mostrar(){
-    
+	public function __construct()
+	{
+		parent::__construct();
+
+
+	}
+
+    Public function mostrarPerfil(){
+        $idUsuario = $_POST["idSeleccionado"];
+        $perfil = New Perfil();
+        $perfilEncontrado = $perfil->buscar($idUsuario);
+        if($perfilEncontrado == NULL){
+            die("No se encontro el perfil");
+        }
+        $this->view("perfil",array());
+    }
 }
+
 
 ?>
---
+++ model/Perfil.php
@@ -0,0 +1,95 @@
+<?php
+class Perfil extends EntidadBase{
+    private $id;
+    private $idUsuario;
+    private $sexo;
+    private $foto;
+
+    
+    public function __construct() {
+        $table="usuarios";
+        parent::__construct($table);
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+    
+    /**
+     * Get the value of idUsuario
+     */ 
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * Set the value of idUsuario
+     *
+     * @return  self
+     */ 
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sexo
+     */ 
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    /**
+     * Set the value of sexo
+     *
+     * @return  self
+     */ 
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of foto
+     */ 
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    /**
+     * Set the value of foto
+     *
+     * @return  self
+     */ 
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+
+    public function buscar($id){
+        $perfilExiste = $this->getById($id);
+        if(!empty($perfilExiste)){
+            return $perfilExiste[0];
+        } else{
+          return NULL;
+        }
+    }
+
+
+    
+}
+?>
